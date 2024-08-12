b6a340d58c43be49fb3a721e4e251ab8d1b8c004
Adam Mauch <maucap20@wfu.edu>
2024-08-12 13:26:08 -0400

    format of homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+// import nav bar, event page, task recap
 
 const Homepage = () => {
+    return (
+        <div className="landing-page">
+          <NavBar />
+          <div className="content">
+            <div className="events">
+                /****/
+            </div>
+            <div className="main">
+                /****/
+            </div>
+            <div className="recap">
+                /****/
+            </div>
+          </div>
+        </div>
+    );
 
 };
 
