6f8dcb1868e52228a1c8a3a3d908145493dd5ac7
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-08-12 09:23:34 -0400

    added mutations for adding and logging in a user in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,33 @@
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
 const resolvers = {
-    Query: {
-      hello: () => 'Hello world!',
+  Query: {},
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-  };
-  
-  module.exports = resolvers;
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
