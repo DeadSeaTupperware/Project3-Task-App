59244ceac9fb816300d44d16df7550ddbcfc403c
Tanner Hicks <tannerhicks@icloud.com>
2024-08-13 17:11:39 -0400

    navbar nonsense

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,42 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img src="/Strive-Logo.png" alt="Logo" className="logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle className="navbar-toggle" aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/landing">Signup</Nav.Link>
-            <Nav.Link as={Link} to="/taskboard">TaskBoard</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-brand">
+          <img src="/Strive-Logo.jpg" alt="Logo" className="logo" />
+        </Link>
+        <button className="navbar-toggle" aria-controls="basic-navbar-nav">
+          ☰
+        </button>
+        <div className="navbar-collapse" id="basic-navbar-nav">
+          <ul className="nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/landing" className="nav-link">Landing</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/taskboard" className="nav-link">TaskBoard</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <button className="dropdown-toggle">More</button>
+              <div className="dropdown-menu">
+                <Link to="#action/3.1" className="dropdown-item">Action</Link>
+                <Link to="#action/3.2" className="dropdown-item">Another action</Link>
+                <Link to="#action/3.3" className="dropdown-item">Something else here</Link>
+                <div className="dropdown-divider"></div>
+                <Link to="#action/3.4" className="dropdown-item">Separated link</Link>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
-
-
 export default BasicExample;
