74be01314b62a081025ba7cc598df97c6fbea219
Adam Mauch <maucap20@wfu.edu>
2024-08-13 09:58:26 -0400

    TaskRecap const

--- client/src/components/TaskRecap.jsx
+++ client/src/components/TaskRecap.jsx
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+const TaskRecap = () => {
+    const [completedTasks, setCompletedTasks] = useState([]);
+  
+    return (
+
+      );
+  };
+  
 
 export default TaskRecap;
