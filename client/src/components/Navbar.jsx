61d455c9ff7cd7af3f94a1ca46d2730fbccad102
Tanner Hicks <tannerhicks@icloud.com>
2024-08-13 19:00:15 -0400

    changed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Menu, Container } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <Menu inverted borderless fixed="top">
+    <Menu inverted borderless fixed="top" className="custom-navbar">
       <Container>
-        <Menu.Item header>
+        <Menu.Item header as={Link} to="/">
         <img src={"/Strive-Logo.png"} alt="Logo" className="logo" />
         </Menu.Item>
-        <Menu.Item name="home" />
-        <Menu.Item name="Task Board" />
+        <Menu.Item as={Link} to="/">
+          Home
+        </Menu.Item>
+        <Menu.Item as={Link} to="/TaskBoard">
+          Task Board
+        </Menu.Item>
         <Menu.Menu position="right">
-          <Menu.Item name="login" />
-          <Menu.Item name="signup" />
-        </Menu.Menu>
-        <Menu.Item header>
-        <img src={"/Strive-Logo.png"} alt="Logo" className="logo" />
+          <Menu.Item as={Link} to="/Login">
+            Login
+          </Menu.Item>
+          <Menu.Item as={Link} to="/Landing">
+            Sign Up
+          </Menu.Item>
+          <Menu.Item header as={Link} to="/">
+          <img src={"/Strive-Logo.png"} alt="Logo" className="logo" />
         </Menu.Item>
+        </Menu.Menu>
       </Container>
     </Menu>
   );
